10045163b142e2e7c2672216fe6133a15c442496
Sincere Tuitt <sinceretuitt@gmail.com>
2024-04-13 17:55:07 -0400

    able to send and receive messages with websockets. messages send to all clients currently

--- src/client/components/ChatContainer.jsx
+++ src/client/components/ChatContainer.jsx
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { createContext, useState } from 'react';
 import SentMessages from './SentMessages.jsx';
 import TypeMessages from './TypeMessage.jsx';
 
 const ChatContainer = () => {
+  // specifies chatroom context - user ids of user who is sending messages and user who is recieving
+  // Logic still has to be implmented to set values of sender and reciever from auth data
+  const [usersData, setUsersData] = useState({
+    sender: '',
+    receiver: '',
+  });
+
+  const socket = new WebSocket('ws://localhost:3000');
+
   return (
     <div>
-      <SentMessages></SentMessages>
-      <TypeMessages></TypeMessages>
+      <SentMessages usersData={usersData} socket={socket}></SentMessages>
+      <TypeMessages usersData={usersData} socket={socket}></TypeMessages>
     </div>
 
-
-  )
-}
+  );
+};
 
 export default ChatContainer;
---
+++ src/server/websocketServer.js
@@ -0,0 +1,33 @@
+/* eslint-disable prefer-arrow-callback */
+/* eslint-disable import/no-extraneous-dependencies */
+const WebSocket = require('ws');
+const express = require('express');
+const http = require('http');
+
+const PORT = 3000;
+
+// create the express server
+const app = express();
+const server = http.createServer(app);
+
+// set up the server to handle websocket connections
+const wss = new WebSocket.WebSocketServer({ server });
+
+// array to store all connected client sockets
+const clients = [];
+
+wss.on('connection', (socket) => {
+  clients.push(socket);
+
+  // for now, just a test event handler to send the same message back to all clients
+  socket.on('message', (e) => {
+    clients.forEach((client) => {
+      client.send(e.toString());
+    });
+  });
+});
+
+// set up the server to handle http requests
+server.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
