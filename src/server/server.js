5f13f68b17deb3c65ace364acc31b90e6fe46c43
Will Suto <will.suto@gmail.com>
2024-04-13 10:14:59 -0700

    catch all route handler and global error handler

--- src/server/server.js
+++ src/server/server.js
@@ -18,6 +18,17 @@ app.use('/api/joke', jokeRouter);
 
 app.use((req, res) => { res.status(404).send('!!Page not found!!'); });
 
+app.use((err, req, res) => {
+  const defaultErr = {
+    log: 'Server error handler caught unknow middleware error',
+    status: 500,
+    message: { err: 'A server error occurred' },
+  };
+  const errorObj = Object.assign(defaultErr, err);
+  console.log(errorObj.log);
+  res.status(errorObj.status).json(errorObj.message);
+});
+
 app.listen(PORT, () => { console.log(`Server listening on ${PORT}`); });
 
 module.exports = app;
