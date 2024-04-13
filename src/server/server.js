1faeebad854e47a1f7a22cbd9f628d8760592ed4
Will Suto <will.suto@gmail.com>
2024-04-13 10:07:17 -0700

    server wip

--- src/server/server.js
+++ src/server/server.js
@@ -1,25 +1,23 @@
 const express = require('express');
-const userController = require('./controllers/userController')
-const path = require('path');
+const jokeRouter = require('./routes/jokeRouter');
+const userRouter = require('./routes/userRouter');
+
 const PORT = 3000;
+
 const app = express();
-const jokeRouter = require('../server/routes/jokeRouter.js')
-const userRouter = require('../server/routes/userRouter.js')
 
-
-
+// parse incoming json
+app.use(express.json());
 
 // serve static files from the build file
 app.use(express.static('build'));
-app.use(express.json());
-// app.get('/', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, '../../dist/bundle.js'));
-// });
 
+// routers
 app.use('/api/user', userRouter);
 app.use('/api/joke', jokeRouter);
 
+app.use((req, res) => { res.status(404).send('!!Page not found!!'); });
 
-app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
+app.listen(PORT, () => { console.log(`Server listening on ${PORT}`); });
 
 module.exports = app;
