17a25eb6e3c7c7d417acf366048f020de3df50b2
Brendan Xiong <32696639+brendanxiong@users.noreply.github.com>
2024-04-18 09:40:45 -0700

    add using json web tokens for auth

--- src/server/controllers/sessionController.js
+++ src/server/controllers/sessionController.js
@@ -1,27 +1,35 @@
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const secretKey = process.env.JWT_SECRET;
+
 const sessionController = {};
 
 sessionController.setSSIDCookieLogin = (req, res, next) => {
     if (res.locals.authenticated === false) return next();
-    const ssid = res.locals.userObj.id;
-    res.cookie('ssid', ssid, { httpOnly: true });
+    const { id } = res.locals.userObj;
+    const token = jwt.sign(id, secretKey);
+    res.cookie('ssid', token, { httpOnly: true, secure: true });
     return next();
-}
+};
 
 sessionController.setSSIDCookieSignUp = (req, res, next) => {
     if (res.locals.userExists === true) return next();
-    const ssid = res.locals.userInfo.id;
-    res.cookie('ssid', ssid, { httpOnly: true });
+    const { id } = res.locals.userInfo;
+    const token = jwt.sign(id, secretKey);
+    res.cookie('ssid', token, { httpOnly: true, secure: true });
     return next();
-}
+};
 
 sessionController.verifySession = (req, res, next) => {
     const { ssid } = res.cookie;
-    if (ssid) return next;
-}
+    if (!ssid) res.send(401).json('unauthorized');
+    res.locals.userID = jwt.verify(ssid, secretKey);
+    return next();
+};
 
 sessionController.removeSSIDCookie = (req, res, next) => {
     res.clearCookie('ssid');
     return next();
-}
+};
 
 module.exports = sessionController;
