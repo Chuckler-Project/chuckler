513ea8b0cafa21b59d7babc1447d0e50c9d9d76f
Will Suto <will.suto@gmail.com>
2024-04-18 18:19:43 -0700

    merging auth changes from remote dev

---
+++ src/client/components/metrics/MetricsPage.jsx
@@ -0,0 +1,18 @@
+import * as Plot from "@observablehq/plot";
+import PlotFigure from "./PlotFigure.js";
+import penguins from "./penguins.json";
+
+const MetricsPage = () {
+  return (
+    <div>
+      <h1>Penguins</h1>
+      <PlotFigure
+        options={{
+          marks: [
+            Plot.dot(penguins, {x: "culmen_length_mm", y: "culmen_depth_mm"})
+          ]
+        }}
+      />
+    </div>
+  );
+}
