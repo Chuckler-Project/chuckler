7cc0a687becedc2efed805529a61ab1de81962fc
Crossur <cruz24955@gmail.com>
2024-04-23 22:21:17 -0400

    fixed chat

--- src/client/components/Chat.jsx
+++ src/client/components/Chat.jsx
@@ -14,7 +14,7 @@ export default function Chat() {
         <div className="background">
             <div className="main-container">
                 <div className='close-chat'>
-                    <Link  to='/main'>x</Link>
+                <input action="action" style={{padding:'0',border:'none',background:'none'}}type="button" value="x" onClick={()=>{window.history.go(-1); return false;}} />
                 </div>
                 <div className="msg-container">
                         <ChatContainer />
