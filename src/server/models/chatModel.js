9e2646d11a94a3321646c356f6fcf090dd23bb13
Richard Gullo <57539853+Xylloz@users.noreply.github.com>
2024-05-01 19:56:09 -0400

    matchModel fleshed out, refactored chat model

--- src/server/models/chatModel.js
+++ src/server/models/chatModel.js
@@ -1,25 +1,28 @@
 const db = require("../../db/db");
 const chatModel = {};
 
-chatModel.getMessages = async (id) => {
-  const getMessagesQuery = await db.query(
-    `SELECT message_content,created_at from messages WHERE from_user_id=$1 LIMIT 10`,
-    [id]
-  );
-  const messagesResult = getMessagesQuery.rows;
+chatModel.getMessages = async (match_id) => {
+  const getMessagesQuery = `
+  SELECT *
+  FROM messages
+  WHERE match_id = $1`;
+
+  const result = await db.query(getMessagesQuery, [match_id]);
+  const messagesResult = result.rows;
   console.log(messagesResult);
   return messagesResult;
 };
 
-chatModel.sendMessage = async (id, message) => {
-  const getMatchQuery = await db.query(
-    "SELECT match_id from matches WHERE user_id_1=$1",
-    [id]
-  ); //will be changed once matchModel.getMatchByUserIds is completed
-  const matchId = getMatchQuery.rows[0]["match_id"];
-  const sendMessageQuery = await db.query(
-    "INSERT INTO MESSAGES (from_user_id,message_content,match_id) VALUES($1,$2,$3)",
-    [id, message, matchId]
-  );
+chatModel.sendMessage = async (match_id, message, from_user_id) => {
+  const sendMessageQuery = `
+  INSERT INTO messages(match_id, message_content, from_user_id) 
+  VALUES($1,$2,$3)`;
+
+  const result = await db.query(sendMessageQuery, [
+    match_id,
+    message,
+    from_user_id,
+  ]);
+  return result.rows[0];
 };
 module.exports = chatModel;
--- src/server/routes/matchRouter.js
+++ src/server/routes/matchRouter.js
@@ -2,15 +2,41 @@ const express = require("express");
 const matchController = require("../controllers/matchController");
 const userController = require("../controllers/userController");
 const tokenController = require("../controllers/tokenController");
+const chatModel = require("../models/chatModel");
 
 const router = express.Router();
 
+// Used to fetch random potential match
+router.post(
+  "/fetch",
+  tokenController.verifyToken,
+  matchController.getPotentialMatch,
+  (req, res) => {
+    return res.status(200).json(res.locals.fetchedUser);
+  }
+);
+
+// TEMP
+router.post(
+  "/messages",
+  tokenController.verifyToken,
+  async (req, res, next) => {
+    try {
+      const { match_id } = req.body;
+      const messages = await chatModel.getMessages(match_id);
+      return res.status(200).json(messages);
+    } catch (err) {
+      return next({});
+    }
+  }
+);
 // Swipe right
 router.post(
   "/like",
   tokenController.verifyToken,
   matchController.likeUser,
   matchController.checkMatch,
+  matchController.createMatch,
   (req, res) => {
     return res.status(200).json(res.locals.message);
   }
@@ -30,8 +56,8 @@ router.post(
 router.post(
   "/unmatch",
   tokenController.verifyToken,
-  matchController.dislikeUser,
   matchController.deleteMatch,
+  matchController.removeLikes,
   (req, res) => {
     return res.status(200).json(res.locals.message);
   }
